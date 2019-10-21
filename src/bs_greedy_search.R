0413dd82cbef80f0d19ad35a314fdf849a994e53
charx <carlos.huertaso@udlap.mx>
2019-10-21 17:38:44 +0200

    functionalized the BS GS algo

--- src/bs_greedy_search.R
+++ src/bs_greedy_search.R
@@ -1,32 +1,38 @@
-rm(list=ls()) # clean R environment
-cat("\f") # clear the console
-
 source('./greedy_search/Greedysearch.R', chdir = TRUE) # source the vanilla greedy search 
-source('./utils.R') # source the utils functions
-set.seed(43) # set seed to the answer of life
 
 library(magrittr) # for the %>% operator 
 library(dplyr)
 library(purrr)
+require(svMisc) # for the progress bar
 
-m_obs = 50 # number of observations
-var_noise = 1 # noise of the data
-
-data = make_test_Data(m_obs, var_noise) # generate data
-
-n_nodes = nrow(data)  #  no. of network nodes
-start_incidence = matrix(0, n_nodes, n_nodes) # Generate an empty incidence matrix
-
-#res = greedy_search(data, start_incidence) # run the greedy search algo
-
-bootTrials = 3 # the amount of bs trials
-bsResults = list()
-for(i in 1:bootTrials) {
-  set.seed(47 + i)
-  # Sample from your data with replacement
-  index       = sample(ncol(data), replace = TRUE)
-  bsData      = data[1:n_nodes, index] # get the data from the bs sampled indexes
+bs_greedy_search = function(data, numIter) {
+  '
+    Will run the Greedy Search Bootstrap algorithm
+    
+    @param data matrix with the rows: num_nodes cols: num obs
+    @param numIter number of iterations that the bootstrap sample will run
+    
+    @return bsResults list containing the best incidence matrix on every 
+    iteration of the GS algorithm
+  '
+  n_nodes = nrow(data)  #  no. of network nodes
+  start_incidence = matrix(0, n_nodes, n_nodes) # Generate an empty incidence matrix
   
-  currBsResults   = greedy_search(bsData, start_incidence) # run a greedy search on the bs sample
-  bsResults[[i]] = currBsResults # append to the bsResults list
+  bootTrials = numIter # the amount of bs trials
+  pbTick = (1 / bootTrials) * 100
+  bsResults = list()
+  progress(0) # inital tick of the pb
+  for(i in 1:bootTrials) {
+    set.seed(47 + i) # for reproducibility
+    # Sample from your data with replacement
+    index       = sample(ncol(data), replace = TRUE)
+    bsData      = data[1:n_nodes, index] # get the data from the bs sampled indexes
+    
+    currBsResults   = greedy_search(bsData, start_incidence) # run a greedy search on the bs sample
+    bsResults[[i]] = currBsResults # append to the bsResults list
+    progress(i * pbTick) # progress bar
+  }
+  
+  return(bsResults) # return the bs list
 }
+
