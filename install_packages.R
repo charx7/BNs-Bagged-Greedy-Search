597b257b07d9f7deb61357fc74fc1b723ec2e0ff
charx <carlos.huertaso@udlap.mx>
2019-10-23 13:30:36 +0200

    added some changes to experimentation

--- install_packages.R
+++ install_packages.R
@@ -3,8 +3,8 @@ install.packages("svMisc") # Progress bar
 install.packages('expm') # package for linear algebra stuff
 install.packages('magrittr') # package for the %>% operator
 install.packages('dplyr')
-install.packages("purrr") # more dependencies
-
+install.packages('purrr') # more dependencies
+install.packages('ggplot2') # for graphs
 # Commands to install the LIMMA package
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
